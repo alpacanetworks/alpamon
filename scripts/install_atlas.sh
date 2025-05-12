cecf33eabb6ef743f09a8cf4c00fd43d52c21816
Apdul0329 <shrmsdn2067@gmail.com>
2025-05-12 13:36:03 +0900

    Fix .goreleaser.yaml & install_atlas.sh
    There was an issue with the existing
    .goreleaser.yaml and install_atlas.sh
    where Linux packages were built based
    on the CPU architecture of the host building
    the Linux package, rather than being packaged
    according to the target GOARCH.
    To resolve this, fix .goreleaser.yaml and install_atlas.sh
    so that Atlas CLI corresponding to the built GOARCH
    is installed.

--- scripts/install_atlas.sh
+++ scripts/install_atlas.sh
@@ -2,12 +2,12 @@
 
 mkdir -p bin
 
-ARCH=${GOARCH}
+ARCH=$1
 
 if [ "$ARCH" = "amd64" ]; then
-  curl -L -o bin/atlas https://release.ariga.io/atlas/atlas-community-linux-amd64-latest
+  curl -L -o "bin/atlas-$ARCH" https://release.ariga.io/atlas/atlas-linux-amd64-latest
 elif [ "$ARCH" = "arm64" ]; then
-  curl -L -o bin/atlas https://release.ariga.io/atlas/atlas-community-linux-arm64-latest
+  curl -L -o "bin/atlas-$ARCH" https://release.ariga.io/atlas/atlas-linux-arm64-latest
 fi
 
-chmod +x bin/atlas
+chmod +x "bin/atlas-$ARCH"
